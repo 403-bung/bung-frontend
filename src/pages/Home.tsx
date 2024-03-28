04a1839a212f4b55a69fdae88de7d1c992727c3a
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-03-28 11:19:00 +0900

    feat: Add home, write, my pages and add navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import AlarmModal from "../components/AlarmModal";
 import { useSelector } from "react-redux";
 import { ToolkitStore } from "../store";
+import NabBar from "../components/NavBar";
 
 export default function Home() {
   const isOpened = useSelector(
@@ -9,8 +10,9 @@ export default function Home() {
 
   return (
     <>
-      <div className="w-[375px] min-h-screen bg-white absolute top-0">
+      <div className="w-[375px] min-h-screen bg-white flex flex-col items-center absolute top-0">
         메인페이지
+        <NabBar />
       </div>
 
       {/* 방금 회원가입한 사람을 대상으로 띄우는 모달*/}
