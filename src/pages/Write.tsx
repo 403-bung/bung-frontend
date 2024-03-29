f71e2449f6dbf9ae2bb1f226dc9f52b7f979f923
yun <austru00@gmail.com>
2024-03-29 16:30:06 +0900

    write page

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,10 +1,107 @@
-import NavBar from "../components/NavBar";
+import { useNavigate } from "react-router-dom";
+import GoBackBtn from "../components/GoBackBtn";
+import Button from "../components/Button";
 
 export default function Write() {
+  const navigate = useNavigate();
   return (
     <>
       <div className="w-[375px] h-full min-h-screen bg-white flex flex-col items-center">
-        <NavBar />
+        {/* 뒤로가기 */}
+        <div className="w-[375px] h-[60px] flex items-center  pl-[9px] pr-[16px] border-b-violet-100 border-b-[1px] bg-white fixed top-0">
+          <GoBackBtn
+            onClick={() => {
+              navigate("/home");
+            }}
+          />
+          <div className="text-[16px] font-[600] ml-[118px]">모임 만들기</div>
+          <div className="text-[16px] font-[600] ml-[79px]">임시저장</div>
+        </div>
+        <div className="mt-[80px] w-[343px] flex flex-col gap-[32px] h-screen ">
+          {/* 모집시간, 모임시작 , 수정하기*/}
+          <div className="flex gap-[32px] flex-col">
+            <div className="flex justify-between ">
+              <div className="text-[16px] font-[400] ">모집 시작</div>
+              <div className="text-[16px] font-[600] ">~ 오후 1시 23분</div>
+            </div>
+            <div className="flex justify-between ">
+              <div className="text-[16px] font-[400] ">모임 시작</div>
+              <div className="text-[16px] font-[600] ">오후 1시 23분~</div>
+            </div>
+            <div className="flex justify-between">
+              <div className="text-[14px] font-[400] px-[8px] py-[4px] rounded-md bg-violet-100 border border-violet-300">
+                수정하기
+              </div>
+            </div>
+          </div>
+          {/* 카테고리 */}
+          <select className="w-full py-[17px] border border-neutral-400  rounded-[10px]  px-[16px] text-zinc-400 text-[16px] font-[600]">
+            <option>카테고리</option>
+            <option>공동구매</option>
+            <option>게임</option>
+            <option>이벤트</option>
+            <option>스터디</option>
+            <option>자유</option>
+            <option>해주세요</option>
+            <option>아이돌</option>
+          </select>
+          {/* 제목 */}
+          <div>
+            <div className="text-[16px] font-[600]">제목</div>
+            <input
+              placeholder="제목을 입력해주세요"
+              className="mt-[8px] px-[16px] py-[17px] rounded-[10px] border border-zinc-600 text-[16px] font-[500] h-[53px] w-[343px]"
+            />
+          </div>
+          {/* 본문 */}
+          <div>
+            <div className="text-[16px] font-[600]">본문</div>
+            <textarea
+              placeholder="내용을 입력해주세요&#10;(예상 시간, 참여 목적 등 구채적으로 적을 수록 &#10;모임 성공 확률이 높아져요)"
+              className="mt-[8px] px-[16px] py-[17px] rounded-[10px] border border-zinc-600 text-[16px] font-[500] h-[200px] w-[343px] leading-tight"
+            />
+          </div>
+          {/* 모집인원 */}
+          <div className="flex items-center gap-[33px]">
+            <div className="text-[16px] font-[600]">모집인원</div>
+            <div>
+              <select className="w-[254px] py-[17px] border border-neutral-400  rounded-[10px]  px-[16px] text-zinc-400 text-[16px] font-[600] ">
+                <option>모집 인원을 입력해주세요</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+              </select>
+            </div>
+          </div>
+          {/* 링크 */}
+          <div>
+            <div className="text-[16px] font-[600]">링크</div>
+            <input
+              placeholder="공유 파일 등 링크를 입력해주세요"
+              className="mt-[8px] px-[16px] py-[17px] rounded-[10px] border border-zinc-600 text-[16px] font-[500] h-[53px] w-[343px]"
+            />
+            <div className="flex mt-[8.5px]">
+              <input type="checkbox" />
+              <div className="text-[14px] font-[400] ml-[8px]">
+                모임 시작 전 비공개
+              </div>
+            </div>
+          </div>
+          {/* 버튼 */}
+          <div>
+            <Button
+              text="작성하기"
+              onClick={() => {
+                navigate("");
+              }}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
