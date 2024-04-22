28b951d218a36f5b99b571609be1b554a0d2b46e
yun <austru00@gmail.com>
2024-04-22 20:26:59 +0900

    animation 수정

--- src/Routes/detail/index.tsx
+++ src/Routes/detail/index.tsx
@@ -9,25 +9,9 @@ import { joinParty } from "api";
 
 export default function Detail() {
   const navigate = useNavigate();
-  const [top, setTop] = useState(0);
-  const [repeatCount, setRepeatCount] = useState(0);
-
   const cookies = new Cookies();
   const userNo = cookies.get("userNo");
   const params = useParams();
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (repeatCount < 3) {
-        setTop((prevTop) => (prevTop === 0 ? -12 : 0));
-        setRepeatCount((prevCount) => prevCount + 1);
-      } else {
-        clearInterval(intervalId);
-      }
-    }, 1000);
-    return () => clearInterval(intervalId);
-  }, [repeatCount]);
-
   return (
     <div className="bg-[#EDE9F6] relative">
       <div className="w-[375px] object-cover bg-[#EDE9F6] flex  flex-col">
@@ -42,10 +26,7 @@ export default function Detail() {
         <div className="flex flex-col justify-center items-center">
           <DetailCard />
           <div className="w-[375px] bottom-0 z-[1] absolute bg-white flex flex-col justify-end items-center py-[54px] ">
-            <div
-              className="w-[181px] h-[32px] p-[4px] bg-violet-100 rounded-3xl transition-transform duration-500 transform "
-              style={{ transform: `translateY(${top}px)` }}
-            >
+            <div className="w-[181px] h-[32px] p-[4px] bg-violet-100 rounded-3xl transition-transform duration-500 transform animate-bounce">
               <div className="h-[24px] flex justify-start items-center ">
                 <div className="bg-indigo-900 px-[7px] pb-[2px] flex rounded-[18px] justify-center items-start">
                   <div className="text-violet-100  font-['Pretendard'] text-[16px]">
